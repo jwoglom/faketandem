7cc7a1b68e59ea63245851ae8e021a3492fbbe4f
James Woglom <j@wogloms.net>
2026-07-12 00:13:13 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while ! ./main -pumpx2-jar-path third_party/pumpx2-cliparser-1.9.0.jar $@
+while ! ./main -pumpx2-jar-path third_party/pumpx2-cliparser-1.9.1.jar $@
 do
   sleep 1
   echo "Restarting program..."
